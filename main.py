284b065082e6efdbe85c8c9876ab7bf12af1ead1
Neha <98503932+nehamittall@users.noreply.github.com>
2022-07-12 17:33:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 st.title("Keyword Extraction using GPT-3")
 input = st.text_area("Input the content whose keywords need to be extracted:")
 
-openai.api_key = "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"
+openai.api_key = "enter your API key here"
 form = st.form("my_form")
 # Now add a submit button to the form:
 form.form_submit_button("submit")
@@ -20,10 +20,3 @@ response = openai.Completion.create(
   presence_penalty=0.0)
 st.text('Keywords:')
 st.text(response["choices"][0]["text"])
-
-
-# steps : OPEN TERMINAL AND WRITE ALL THIS:
-# cd programming/python/ps1\(1\)
-# source ./env/bin/activate
-# streamlit run /Users/neha/programming/python/ps1\(1\)/main.py
-
