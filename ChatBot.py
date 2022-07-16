75b42fbb390db590156f4fd088dbf409b7e84888
dhavalmaniar05 <dhavalmaniar05@gmail.com>
2022-07-16 21:00:42 +0530

    update chatbot

--- ChatBot.py
+++ ChatBot.py
@@ -1,33 +1,41 @@
 import streamlit as st
 import openai
 from streamlit_chat import message
+from dotenv import load_dotenv
+import os
 
-openai.organization = "Org_Key" #Use your Organization Key here
-openai.api_key ="API_Key"  #Use your API-Key here
+load_dotenv()
+openai.organization = "org-2Mok0YRnTfHOVbx3EJXTXZOu"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-if "history" not in st.session_state:
+if 'history' not in st.session_state:
     st.session_state['history'] = []
     st.session_state['history'].append({"message": "Hello", "is_user": False})
+
 
 def Generate_Response():
     user_message = st.session_state.input_text
 
     response = openai.Completion.create(
-        model = "text-davinci-002",            
-        prompt = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: " + user_message + "\nA:",
-        temperature = 0.0,
-        top_p = 1.0,
-        frequency_penalty = 0.0,
-        presence_penalty = 0.0,
-        stop=["\n"] 
-        )
+        model="text-davinci-002",
+        prompt="Q:What is obsessive-compulsive personality disorder?\nA:Obsessive-compulsive personality disorder (OCPD) is a personality disorder that is characterized by a rigid adherence to rules and regulations, an obsession with details and orderliness, and a need for control. People with OCPD often have difficulty completing tasks and may be overly critical of others.\n\nQ: What is obsessive-compulsive personality disorder?\nA: Obsessive-compulsive personality disorder (OCPD) is a personality disorder that is characterized by a rigid adherence to rules and regulations, an obsession with details and orderliness, and a need for control. People with OCPD often have difficulty completing tasks and may be overly critical of others.\n\nQ:What is antisocial personality disorder? \nA:Antisocial personality disorder is a personality disorder characterized by a lack of empathy and a disregard for the rights of others. People with this disorder often engage in criminal behavior and may be manipulative or callous.\n\nQ: " + user_message + "\nA:",
+        temperature=0.25,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.25,
+        presence_penalty=0.15,
+        stop=["\n"]
+    )
 
-    st.session_state['history'].append({"message": user_message, "is_user": True})
-    st.session_state['history'].append({"message": response['choices'][0]['text'], "is_user": False})
+    st.session_state['history'].append(
+        {"message": user_message, "is_user": True})
+    st.session_state['history'].append(
+        {"message": response['choices'][0]['text'], "is_user": False})
 
-st.text_input("You:", key = "input_text", on_change = Generate_Response)
+
+st.text_input("You:", key="input_text", on_change=Generate_Response)
 
 count = 0
 for chat in reversed(st.session_state["history"]):
-    message(**chat, key = count)
+    message(**chat, key=count)
     count += 1
